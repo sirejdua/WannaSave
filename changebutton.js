2290c304225b5b59f8d4a092b756c7e404d26874
Connor Prisby <connor.prisby@gmail.com>
2017-09-15 23:28:41 -0700

    Corrected all errors

--- changebutton.js
+++ changebutton.js
@@ -1,15 +1,16 @@
-var plus_2_cart = document.getElementsByClassName("a-button-stack")[0];
-
 try {
+	var plus_2_cart = document.getElementsByClassName("a-button-stack")[0];
+
 	var brand = document.getElementById("brand").text.replace(/\s/g,'');
-}
-catch (e) {
-	var brand = ""
-}
+	if (brand != "Intuit") {
 
-if (brand != "Intuit") {
-
-	for (var i = 0; i < 2; i++) {
-		plus_2_cart.remove();
+		for (var i = 0; i < 2; i++) {
+			plus_2_cart.remove();
+		}
 	}
 }
+catch (e) {
+	
+}
+
+
