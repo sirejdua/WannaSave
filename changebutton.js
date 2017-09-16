1bd859415aec53002daf8d40fa2b50af2a7a7512
Connor Prisby <connor.prisby@gmail.com>
2017-09-16 00:18:05 -0700

    Solves world hunger

--- changebutton.js
+++ changebutton.js
@@ -1,19 +1,24 @@
-var plus_two_cart_arg = document.getElementsByClassName("a-button-stack");
-try {
-	var brand = document.getElementById("brand").text.replace(/\s/g,'');
-}
-catch(err) {
-	var brand = "";
-}
-if (brand != "Intuit") {
-	for (var i = 0; i < plus_two_cart_arg.length; i++) {
-		plus_two_cart_arg[i].remove();
-		try {
+function delete_buy() {
+	var plus_two_cart_arg = document.getElementsByClassName("a-button-stack");
+	//window.alert(plus_two_cart_arg.length);
+	try {
+		var brand = document.getElementById("brand").text.replace(/\s/g,'');
+	}
+	catch(err) {
+		var brand = "";
+	}
+	if (brand != "Intuit") {
+		for (var i = 0; i < plus_two_cart_arg.length; i++) {
 			plus_two_cart_arg[i].remove();
+			try {
+				plus_two_cart_arg[i].remove();
+			}
+			catch(err) {
+				//nop
+			}
 		}
-		catch(err) {
-			//nop
-		}
-		
 	}
 }
+
+delete_buy();
+delete_buy();
