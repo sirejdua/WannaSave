0aca7cb8762ef42862eb06f786c56c116cc2a898
Connor Prisby <connor.prisby@gmail.com>
2017-09-15 23:31:14 -0700

    Actually fixed all errors

--- changebutton.js
+++ changebutton.js
@@ -1,16 +1,22 @@
 try {
 	var plus_2_cart = document.getElementsByClassName("a-button-stack")[0];
 
-	var brand = document.getElementById("brand").text.replace(/\s/g,'');
-	if (brand != "Intuit") {
+	try {
+		var brand = document.getElementById("brand").text.replace(/\s/g,'');
+		if (brand != "Intuit") {
 
+			for (var i = 0; i < 2; i++) {
+				plus_2_cart.remove();
+			}
+		}
+	}
+	catch (e) {
 		for (var i = 0; i < 2; i++) {
 			plus_2_cart.remove();
 		}
 	}
-}
-catch (e) {
 	
 }
+catch (e) {
 
-
+}
